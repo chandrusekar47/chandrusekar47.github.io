c855525034ee117dfebd6939e63e0d0bb039eb8a
Chandrasekar <2032697+chandrusekar47@users.noreply.github.com>
2025-12-06 01:58:19 +0000

    Remove all files except index.html

--- artist-migration/scatter/data.js
+++
@@ -1,9 +0,0 @@
-window.Data = (() => {
-	return {
-		fetch: function (callback) {
-			$.get({url:"data/birth_death_counts_padded.json", dataType:'json'})
-			.done(callback)
-			return this
-		}
-	}
-})()
--- historical-analysis-mlb/data.js
+++
@@ -1,9 +0,0 @@
-window.Data = (() => {
-	return {
-		fetch: function (callback) {
-			$.get({url:"summary.json", dataType: 'json'})
-			.done(callback)
-			return this
-		}
-	}
-})()
